48a1d5ba1d44d64c76ad1e225eeea8d2d998fe44
Jeremy Jones (scjoneje) <jeremy.jones@s1now.com>
2010-06-18 16:58:08 +0100

    add

---
+++ dotmailer.py
@@ -0,0 +1,123 @@
+#!/bin/env python
+
+"""
+
+SOAP client to query the dotmailer account and pull back any results
+from campaign sends (e.g. hard bounces etc) in JSON format.
+
+Usage: dotmailer.py <action> [<data>]
+
+Where action is one of 'query' 'hardbounces' or 'listcampaigns' and
+data is appropriate to the action (e.g. for 'query' you provide an
+email address etc).
+
+"""
+
+import sys
+
+from suds.client  import Client     as SOAPClient
+from django.utils import simplejson as json
+from types        import *
+
+from dotmailersupport import api_username, api_password
+
+url              = r'http://apiconnector.com/api.asmx?WSDL'
+api_username     = api_username()
+api_password     = api_password()
+
+try:
+    action           = sys.argv[1]
+except IndexError:
+    print "Usage: dotmailer <action> [<data>]"
+    sys.exit(1)
+
+def printContactResult (result):
+    myUser = {}
+        
+    for dataP in result:
+        if type(dataP[1]) is not InstanceType:
+            myUser[dataP[0]] = dataP[1]
+
+    for fieldIdx in range(len(result.DataFields['Keys']['string'])):
+        myUser["DATAFIELD--" + result.DataFields['Keys']['string'][fieldIdx]] = result.DataFields['Values'][0][fieldIdx]
+
+    s = json.dumps(myUser, sort_keys=True, indent=4)
+    print '\n'.join([l.rstrip() for l in  s.splitlines()])
+
+def getMyCampaigns ():
+    
+    client      = SOAPClient(url)
+
+    try:
+        result = client.service.ListCampaigns(api_username, api_password)
+    except:
+        print "Campaign list fail"
+    else:
+
+        myCampaigns = []
+
+        for campaign in result[0]:
+            myCamp = {}
+            for r in campaign:
+                myCamp[r[0]] = r[1]
+
+            myCampaigns.append(myCamp)
+
+        return myCampaigns
+
+#---
+
+if action == 'query':
+
+    client        = SOAPClient(url)
+    try:
+        user_email    = sys.argv[2]
+    except IndexError:
+        print "Usage: dotmailer query <email_address>"
+        sys.exit(1)
+    else:
+        try:
+            result = client.service.GetContactByEmail(api_username, api_password, user_email)
+        except:
+            print "User " + user_email + " not found"
+        else:
+            printContactResult(result)
+        
+elif action == 'hardbounces':
+
+    client      = SOAPClient(url)
+    myUsers     = []
+    myCampaigns = []
+
+    try:
+        campaignWanted = sys.argv[2]
+    except IndexError:
+        myCampaigns = getMyCampaigns()
+    else:
+        myCampaigns.append( {"Id": campaignWanted} )
+
+    for campaign_idx in range(len(myCampaigns)):
+        campaign_id = myCampaigns[campaign_idx]['Id']
+
+        try:
+            result = client.service.ListHardBouncingContacts(api_username, api_password, campaign_id)
+        except:
+            print "Campaign " + campaign_id + " not found"
+        else:
+            for bouncedContact in result[0]:
+                userEmailAddress = bouncedContact[1]
+                myUsers.append(userEmailAddress)
+            
+    s = json.dumps(myUsers, sort_keys=True, indent=4)
+    print '\n'.join([l.rstrip() for l in  s.splitlines()])
+
+elif action == 'listcampaigns':
+    
+    myCampaigns = getMyCampaigns()
+
+    s = json.dumps(myCampaigns, sort_keys=True, indent=4)
+    print '\n'.join([l.rstrip() for l in  s.splitlines()])
+
+else:
+
+    print "Action '" + action + "' not found."
---
+++ dotmailersupport.sample.py
@@ -0,0 +1,12 @@
+
+"""
+
+Sample support file for dotmailer command-line utility.
+
+"""
+
+def api_username():
+    return r'apiuser@apiconnector.com'
+
+def api_password():
+    return r'secretpassword'
